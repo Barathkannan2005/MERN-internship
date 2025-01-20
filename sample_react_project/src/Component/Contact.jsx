cafd62b131fd9750fdf674fdd0ff5de0bc067d3a
Barathkannan2005 <barath.s2023cse@sece.ac.in>
2025-01-20 18:44:34 +0530

    appjsx

---
+++ sample_react_project/src/Component/Contact.jsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+    var a = 10;
+    return (
+        <div>Welcome to contact page
+        <h3>this is a state example</h3>
+        <h4>{a} </h4>
+        <button>+</button>
+        </div>
+    );
+};
+export default Contact;
---
+++ sample_react_project/src/Component/gallery.jsx
@@ -0,0 +1,9 @@
+const Gallery = (sece) => {
+    return(
+        <div>
+            <h2>welcome to {sece.page} page</h2>
+            < img src="logo.png" alt
+        </div>
+    )
+    q
+}
